cd6e3baa235ee1bede09f69e538e108355aa1b20
THIRUCHENTHILL-S-P <123857038+THIRUCHENTHILL-S-P@users.noreply.github.com>
2024-06-21 09:54:43 +0530

    Add files via upload

---
+++ arithmetic_calculator.py
@@ -0,0 +1,5 @@
+a = input("Enter the operand 1           : ")
+b = input("Enter the operand 2           : ")
+c = input("Enter the arithmetic operator : ")
+eval_stat = a + " " + c + " " + b
+print(eval_stat, " = ", eval(eval_stat))
